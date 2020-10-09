77d3683670381b0508f7b850631ee44066017cb1
Lucas Amaral <lucas.amaral@edge.ufal.br>
2020-10-09 19:18:31 -0300

    Copiar arquivos e baixar novamente os videos faltosos da base

--- copy_2_folders_new_downloads.py
+++ copy_2_folders_new_downloads.py
@@ -2,28 +2,38 @@
 import os
 from tqdm import tqdm
 import shutil
+from urllib.parse import unquote
 import pandas as pd
 
 #%%
-bad_videos = pd.read_csv('bad_videos.csv')
+bad_videos = pd.read_csv('need_2_download.csv')
 db_path = '/media/usuario/Others/gdrive/LibrasCorpus/Santa Catarina/Inventario Libras/'
-base_download_folder = '/home/usuario/Downloads/'
+base_download_folder = '/home/usuario/Downloads/libras_videos/'
 downloads_files = list(filter(lambda x: '.mp4' in x ,
                               [os.path.join(base_download_folder, x) for x in os.listdir(base_download_folder)]))
 
 all_folders_path = sorted([os.path.join(db_path, x) for x in os.listdir(db_path)],
                           key=lambda x: int(x.split(' v')[-1]), reverse=True)
+bad_videos['vid_name'] = bad_videos.name
 
 
 # %%
 for row in tqdm(bad_videos.iterrows(), total=bad_videos.shape[0]):
     row = row[1]
-    only_name = row.video_name.replace('\\', '/').split('/')[-1]
-    vpart_folder = row.video_name.replace('\\', '/').split('/')[-2]
+
+    only_name = row.vid_name.replace('\\', '/').split('/')[-1]
+    vpart_folder = row.vid_name.replace('\\', '/').split('/')[-2]
 
     download_file_path = os.path.join(base_download_folder, only_name)
 
-    dst_file_path = os.path.join(db_path, vpart_folder, only_name)
+    download_file_path = unquote(download_file_path)
 
-    if os.path.exists(download_file_path) and os.path.exists(dst_file_path):
+    is_eaf = 'EAF' in download_file_path
+    if is_eaf:
+        download_file_path = download_file_path[:-3] + "txt"
+
+    dst_file_path = os.path.join(db_path, vpart_folder, only_name)
+    dst_file_path = unquote(dst_file_path)
+
+    if os.path.exists(download_file_path):
         shutil.copy(download_file_path, dst_file_path)
---
+++ get_files_url_not_downloaded.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+# %%
+proj_path = '/media/usuario/Others/gdrive/LibrasCorpus/Santa Catarina/Inventario Libras/'
+items = os.listdir(proj_path)
+items = list(map(lambda x: os.path.join(proj_path, x), items))
+to_download = pd.DataFrame()
+for k, it in tqdm(enumerate(items)):
+    vpart = it.split(' v')[-1]
+    csv_path = list(filter(lambda x: 'download' in x, os.listdir(it)))
+    if len(csv_path) == 0:
+        continue
+
+    csv_path = csv_path[0]
+    csv_path = os.path.join(it, csv_path)
+    csv_file = pd.read_csv(csv_path, index_col=0)
+    for row in csv_file.iterrows():
+        row = row[1]
+        name = row.files.split('/')[-1] if 'mp4' in row.files else row.files.split('?')[0].split('/')[-1] + '.EAF'
+        if name.find('mp4') != -1:
+            idx = name.find('mp4')
+            name = name[:idx + 3]
+
+        file_url = row.files if row.video == 1 else 'http://' + row.files
+        file_name = os.path.join(it, name)
+        if not os.path.exists(file_name):
+            to_download = to_download.append(pd.DataFrame(dict(
+                url=[file_url], name=[file_name], vpart=[vpart]
+            )), ignore_index=True)
+to_download.to_csv('need_2_download.csv', index=False)
+# %%
+for a in to_download.iterrows():
+    a = a[1]
+    print(a.url, file=open('to_download.txt', mode='a'))
